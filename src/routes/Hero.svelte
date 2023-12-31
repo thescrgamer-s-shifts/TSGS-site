7fb5998594f0e5c5bd94c1caa200dc025fb900d4
TheHackerChampion <felix.schweikl@hgtb.de>
2023-12-31 16:40:11 +0100

    new hero

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,28 +1,10 @@
-<div class="hero">
-    <div class="container">
-        <h2>Welcome to thescrgamer's shifts!</h2>
-        <p>
-        Your Discord server for roblox game shifts
-        </p>
-    </div>
-    <div class="container">
-        <img src="" alt="">
-    </div>
-</div>
+<img src="static/header.png" alt="">
 
 <style>
-    .hero {
-        padding: 5em 0;
-        background-color: var(--primary-color);
-        color: var(--text-color);
+    img {
+        width: 1000px;
+        margin-left: 23%;
+        margin-top: 10px;
     }
-    h2 {
-        margin: 0;
-        font-size: 3em;
-        font-weight: 500;
-    }
-    p {
-        font-size: 1.1em;
-        line-height: 1.4em;
-    }
+    
 </style>
